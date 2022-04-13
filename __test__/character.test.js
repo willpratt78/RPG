58e30567673144cf00f5c9a48ba0685cdd276927
William Pratt <willpratt78@gmail.com>
2022-04-13 11:22:24 -0700

    code to determine class of character

---
+++ __test__/character.test.js
@@ -0,0 +1,102 @@
+import { Character } from "../src/character.js";
+
+describe ('Character', () => {
+
+  test('should return Character object', () => {
+    const info = new Character("wizard"); 
+    console.log(info);
+    expect(info.type).toEqual("wizard");
+  })
+})
+
+
+
+  // test('should be defined by selected class', () => {
+  //   const characterName = new Character(); 
+  //   const wizard = "Jeff"
+  //   characterName.push(wizard)
+  // })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
+
+// Describe ('Constructor name', () => {
+
+//   test('should accuratly describe what the test will be doing once passing', () => {
+    
+//   })
+// })
---
+++ src/character.js
@@ -0,0 +1,40 @@
+export function Character(type, weapon, shield) {
+this.weapon = weapon;
+this.shield = shield;
+this.type = type;
+// this.getDefense();
+// this.getAttack();
+  }
+  
+  
+  // Character.prototype.addWeapon = function(weapon) {
+  //   this.attack.push(weapon);
+  // }
+//   Character.prototype.addShield = function(shield) {
+//     this.shield.push(shield);
+
+   Character.prototype.getDefense = function() {
+      this.defense = 0;
+
+      if (this.type === "wizard") {
+        this.defense = 5 ;
+      } else if (this.type === "warrior") {
+        this.defense = 10;
+      } else { 
+      this.defense= 15;
+      }
+ this.defense += this.shield;
+}
+//      Character.prototype.getAttack = function() {
+//        this.attack = 0;
+    
+//         if (this.type === "wizard") {
+//           this.attack = 15 ;
+//         } else if (this.type === "warrior") {
+//           this.attack = 10;
+//         } else { 
+//         this.attack = 5;
+//         }
+//     this.attack += this.weapon;
+//   };
+// };
