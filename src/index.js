d2b606d6cd9048a9acac78e884244d9432b8e0f8
William Pratt <willpratt78@gmail.com>
2022-04-14 13:47:11 -0700

    base jquery script info

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import { Character } from "../src/character";
-import { Battle } from "../src/battle";
+import { Character, NonPlayerCharacter } from "../src/character";
+// import { Battle } from "../src/battle";
 
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+
+
+function CreateChar (isTurn, type, weapon) {
+  let info = new Character(true, "#type", "#weapon");
+}
+
+function NPC (type, weapon, isTurn) {
+  let npc = new NonPlayerCharacter("wizard", "stick", false)
+}
+
+$( "#charCreation" ).on(function () {
+  const newChar = CreateChar;
+  const npc = NPC;
+})
